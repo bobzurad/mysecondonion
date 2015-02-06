f77f0876e92a6841fdd01f56f4c8888c6ee7343d
bobzurad <bob@zurad.net>
2015-02-05 22:28:57 -0600

    added unit and integration tests

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 mysecondonion
 =============
-My second example of Onion Architecture. Here we use the Generic Repository and Unit of Work patterns implemented with ASP.NET MVC, Entity Framework, Autofac, and Moq.
+My second example of Onion Architecture. Here we use the Generic Repository and Unit of Work patterns implemented using ASP.NET MVC, Entity Framework, Autofac, and Moq.
 
-Inspired by http://techbrij.com/generic-repository-unit-testing-ioc-di-asp-net-mvc
+This started out as the example found [here](http://techbrij.com/generic-repository-unit-testing-ioc-di-asp-net-mvc)
 
-Things I've changed, or what I'm doing differently:
-* Repository project is named Data
-* Model project is named Domain
+I've since made the following additions/changes:
 * EF configuration is code based, rather than using attributes on models
 * All interfaces (with the exception of ISampleArchContext) live in the Domain
-
+* Repositories take a SampleArchContext instead of a DbContext
+* TODO: fix repository tests
---
+++ SampleArch.Test/Service/CountryServiceTests.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SampleArch.Domain;
+using SampleArch.Domain.Repository;
+using SampleArch.Domain.Service;
+using SampleArch.Domain.UnitOfWork;
+using SampleArch.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleArch.Test.Service
+{
+    [TestClass]
+    public class CountryServiceTest
+    {
+        private Mock<ICountryRepository> _mockRepository;
+        private ICountryService _service;
+        Mock<IUnitOfWork> _mockUnitWork;
+        List<Country> listCountry;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _mockRepository = new Mock<ICountryRepository>();
+            _mockUnitWork = new Mock<IUnitOfWork>();
+            _service = new CountryService(_mockUnitWork.Object, _mockRepository.Object);
+            listCountry = new List<Country>() {
+           new Country() { Id = 1, Name = "US" },
+           new Country() { Id = 2, Name = "India" },
+           new Country() { Id = 3, Name = "Russia" }
+          };
+        }
+
+        [TestMethod]
+        public void Country_Get_All()
+        {
+            //Arrange
+            _mockRepository.Setup(x => x.GetAll()).Returns(listCountry);
+
+            //Act
+            List<Country> results = _service.GetAll() as List<Country>;
+
+            //Assert
+            Assert.IsNotNull(results);
+            Assert.AreEqual(3, results.Count);
+        }
+
+        [TestMethod]
+        public void Can_Add_Country()
+        {
+            //Arrange
+            int Id = 1;
+            Country emp = new Country() { Name = "UK" };
+            _mockRepository.Setup(m => m.Add(emp)).Returns((Country e) =>
+            {
+                e.Id = Id;
+                return e;
+            });
+
+            //Act
+            _service.Create(emp);
+
+            //Assert
+            Assert.AreEqual(Id, emp.Id);
+            _mockUnitWork.Verify(m => m.Commit(), Times.Once);
+        }
+    }
+}
